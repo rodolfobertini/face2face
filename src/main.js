907fa09bef38b78f428019dec0fe9ed8ead355b3
Rodolfo Bertini <rodolfobr@gmail.com>
2023-11-03 12:44:45 -0300

    counter

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 import './style.css'
 import typescriptLogo from './typescript.svg'
 import viteLogo from '/vite.svg'
-import { setupCounter } from './counter'
 import { $ } from './select';
 import { logo } from './components/logo';
 import { counter } from './components/counter';
